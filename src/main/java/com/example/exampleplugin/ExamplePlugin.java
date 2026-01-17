c71f6659ca46e4d5be2be9b6bc31edb805f53323
Nyte <rasmusrajasoo@hotmail.com>
2026-01-17 12:14:28 +0100

    Lifesteal Method Implemented

--- src/main/java/com/example/exampleplugin/ExamplePlugin.java
+++ src/main/java/com/example/exampleplugin/ExamplePlugin.java
@@ -1,8 +1,10 @@
 package com.example.exampleplugin;
 
+import com.hypixel.hytale.component.ComponentRegistryProxy;
 import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 
 public class ExamplePlugin extends JavaPlugin {
     private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
@@ -14,6 +16,10 @@ public class ExamplePlugin extends JavaPlugin {
 
     @Override
     protected void setup() {
+        ComponentRegistryProxy<EntityStore> registry = getEntityStoreRegistry();
+        registry.registerSystem(new LifestealSystems.LifestealOnDamage());
+
+        // Register any other systems here
         this.getCommandRegistry().registerCommand(new ExampleCommand(this.getName(), this.getManifest().getVersion().toString()));
     }
 }
